c41d28998fb3fa1f56726759560899e7dbeb22b8
Giu13sse <72180405+Giu13sse@users.noreply.github.com>
2021-03-28 00:00:57 -0500

    Add files via upload

---
+++ De Flecha/Distancia.js
@@ -0,0 +1,7 @@
+x1=8
+x2=10
+y1=20
+y2=30
+var distancia= Math.sqrt(Math.pow((Math.pow((x2-x1),2)+(y2-y1)),2))
+
+console.log("La distancia es " + distancia)
---
+++ Funciones/distancia.js
@@ -0,0 +1,10 @@
+x1=8
+x2=10
+y1=20
+y2=30
+const x= Math.pow((x2-x1),2)
+const y= y2-y1
+var root = Math.pow((x+y),2)
+var distancia1= Math.sqrt(root)
+
+console.log("La distancia es " + distancia1)
