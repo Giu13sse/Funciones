9002d5f0fd7ef9937edee2b62c53b5f97f0d5915
Giu13sse <72180405+Giu13sse@users.noreply.github.com>
2021-03-28 21:01:02 -0500

    Update split.js

--- De Flecha/split.js
+++ De Flecha/split.js
@@ -1,1 +1,12 @@
 
+let nave1= 'ARQ255: Sara Bel-Sun'
+let nave2='ARQ2556: Nodin Chavdri'
+let nave3='ARQ2557: Finn'
+
+
+var splitname= (string) => {split=string.split(":");let shipalone=split[0];let splitspring= split[1];return splitspring }
+
+console.log(splitname(nave1));
+console.log(splitname(nave2));
+console.log(splitname(nave3));
+//
