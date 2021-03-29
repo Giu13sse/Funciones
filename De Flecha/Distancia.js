dd3509991cc238b0722ccfdaae7ee7783a80e16b
Giu13sse <72180405+Giu13sse@users.noreply.github.com>
2021-03-28 20:39:54 -0500

    Update Distancia.js

--- De Flecha/Distancia.js
+++ De Flecha/Distancia.js
@@ -1,9 +1,11 @@
-x1=8
-x2=10
-y1=20
-y2=30
-var distancia= Math.sqrt(Math.pow((Math.pow((x2-x1),2)+(y2-y1)),2))
+var x1=8;
+var x2=10;
+var y1=20;
+var y2=30;
+let distancia= (n1,n2,d1,d2) => Math.sqrt(Math.pow((Math.pow((n2-n1),2)+(d2-d1)),2))
 
-console.log("La distancia es " + distancia)
+console.log("La distancia es " + distancia(x1,x2,y1,y2))
+
 
 //la funcion usada es la que esta en el taller que el cuadrado es tomado de la resta de las x y cuadrado de la suma del cuadrado de las x mas la resta de las y
+
